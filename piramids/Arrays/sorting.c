e6d58d0b07fcd871712db992c3eff935d600b9a7
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2025-02-03 19:46:06 -0500

    hello world cpp

--- piramids/Arrays/sorting.c
+++ piramids/Arrays/sorting.c
@@ -44,6 +44,45 @@ void sort_strings(int n, char *input[n], char *output[n]){
    }
 }
 
+int *scan_right(int *left, int *right){
+   while(left < right){
+      if(*left >= 0) break;
+      left++;
+   }
+   return left;
+}
+
+int *scan_left(int *left, int *right){
+   while (left < right){
+      if(right < 0) break;
+      right--;
+   }
+   return right;
+}
+
+void swap(int *left, int *right){
+   int i = *left;
+   *left = *right;
+   *right = i;
+}
+
+int split(int n, int array[n]){
+   int *left = array, *right = array + n - 1;
+   while (left < right){
+      left = scan_right(left, right);
+      right = scan_left(left, right);
+      swap(left, right);
+   }
+   return left - array;
+}
+
+void reverse(int n, int array[n]){
+   int *left = array, *right = array + n - 1;
+   while (left < right){
+      swap(left++, right--);
+   }
+}
+
 void bucket_sort(int n, int offset, int const input[n], int output[n]){
    int buckets[256];
    for (int i = 0; i < 256; i++){
@@ -76,13 +115,29 @@ void radix_sort(int n, int array[n]){
       bucket_sort(n, offset, buffers[bucket_input], buffers[!bucket_input]);
       bucket_input = !bucket_input;
    }
-   
+}
+
+void sort_int(int n, int array[n]){
+   if(n <= 0) return;
+   int m = split(n, array);
+   printf("m == %d\n", m);
+   for (int i = 0; i < n; i++){
+      printf("%d ", array[i]);
+   }
+   printf("\n");
+   if(m > 0){
+      radix_sort(m, array);
+      reverse(m, array);
+   }
+   if(m < n){
+      radix_sort(n - m, array + m);
+   }
 }
 
 int main(){
    unsigned char array[] = { 'f', 'o', 'o', 'b', 'a', 'r' };
    char *array1[] = {"foo", "boo", "bar", "qoo", "qar", "baz", "qux", "qar", "123"};
-   int array2[] = { -1, -2, 13, 12, 13, 6, 14, -3, 42, 13 };
+   int array2[] = { -1, -2, 13, -2, 13, 6, 14, -3, 42, 13, 23 };
    int n = sizeof array1 / sizeof *array1;
    int n1 = sizeof array2 / sizeof *array2;
 
@@ -94,6 +149,7 @@ int main(){
       printf("%s\n", output[i]);
    }
    radix_sort(n1, array2);
+   sort_int(n1, array2);
    for (int i = 0; i < n1; i++){
       printf("%d ", array2[i]);
    }

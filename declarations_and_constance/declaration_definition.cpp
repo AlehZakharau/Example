887bf83f550c6dc676893206804bc876ed6de16c
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2025-02-05 13:13:30 -0500

    declaration and definition

---
+++ declarations_and_constance/declaration_definition.cpp
@@ -0,0 +1,24 @@
+
+char ch;
+int count = 2;
+char* name = "Aleh";
+struct complex {float x, y;};
+complex cvar;
+extern complex sqrt(complex p) { int a = p.x*p.y;};
+extern int error_number = -1;
+typedef complex point;
+float read(complex* p) {return p->y;}
+const double pi = 3.141592;
+struct user {char name;};
+template<class T> int abs(T a) {return a<0 ? -a : a;}
+enum beer {Carlsberg, Tuborg, Thor};
+
+int count;
+float read(complex*);
+
+
+
+
+int main(){
+
+}
